449369a7c0c75b224a406f22312d3a76c0f35687
Ardana <79736227+ardanarx@users.noreply.github.com>
2022-05-02 21:23:32 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -5,18 +5,14 @@
 #
 # You can find misc modules, which dont fit in anything xD
 """ Userbot module for other small commands. """
-
 import io
 import sys
 from os import execl
 from random import randint
 from time import sleep
-
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
 from userbot.events import register
 from userbot.utils import time_formatter
-
-
 @register(outgoing=True, pattern="^.random")
 async def randomise(items):
     """ For .random command, get a random item from the list of items. """
@@ -30,8 +26,6 @@ async def randomise(items):
     await items.edit(
         "**Query: **\n`" + items.text[8:] + "`\n**Output: **\n`" + itemo[index] + "`"
     )
-
-
 @register(outgoing=True, pattern="^.sleep ([0-9]+)$")
 async def sleepybot(time):
     """ For .sleep command, let the userbot snooze for a few second. """
@@ -45,8 +39,6 @@ async def sleepybot(time):
         )
     sleep(counter)
     await time.edit("`OK, I'm awake now.`")
-
-
 @register(outgoing=True, pattern="^.shutdown$")
 async def killbot(shut):
     """For .shutdown command, shut the bot down."""
@@ -54,8 +46,6 @@ async def killbot(shut):
     if BOTLOG:
         await shut.client.send_message(BOTLOG_CHATID, "#SHUTDOWN \n" "Bot shut down")
     await bot.disconnect()
-
-
 @register(outgoing=True, pattern="^.restart$")
 async def killdabot(reboot):
     await reboot.edit("`*i would be back in a moment*`")
@@ -66,13 +56,12 @@ async def killdabot(reboot):
     execl(sys.executable, sys.executable, *sys.argv)
     # Shut the existing one down
     exit()
-
-
 @register(outgoing=True, pattern="^.readme$")
 async def reedme(event):
     await event.edit(
         "Here's something for you to read:\n"
         "\n[One4uBot's README.md file](https://github.com/MoveAngel/One4uBot/blob/sql-extended/README.md)"
+        "\n[One4uBot's README.md file](https://github.com/ardanarx/Userbot/blob/sql-extended/README.md)"
         "\n[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-07-01-2)"
         "\n[Setup Guide - Google Drive](https://telegra.ph/How-To-Setup-Google-Drive-04-03)"
         "\n[Setup Guide - LastFM Module](https://telegra.ph/How-to-set-up-LastFM-module-for-Paperplane-userbot-11-02)"
@@ -81,28 +70,22 @@ async def reedme(event):
         "\n[Instant Setup - Generate String Session](https://repl.it/@MoveAngel/UserbotSession?lite=1&outputonly=1)"
         "\n*For Instant Setup, click the green button called 'run' before use"
     )
-
-
 # Copyright (c) Gegham Zakaryan | 2019
 @register(outgoing=True, pattern="^.repeat (.*)")
 async def repeat(rep):
     cnt, txt = rep.pattern_match.group(1).split(" ", 1)
     replyCount = int(cnt)
     toBeRepeated = txt
-
     replyText = toBeRepeated + "\n"
-
     for i in range(0, replyCount - 1):
         replyText += toBeRepeated + "\n"
-
     await rep.edit(replyText)
-
-
 @register(outgoing=True, pattern="^.repo$")
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
         "[Click here](https://github.com/MoveAngel/One4uBot) to open One4uBot's GitHub page."
+        "[Click here](https://github.com/ardanarx/Userbot) to open One4uBot's GitHub page."
     )
 
 
@@ -128,22 +111,18 @@ async def raw(rawtext):
             reply_to=reply_to_id,
             caption="`Here's the decoded message data !!`",
         )
-
-
 CMD_HELP.update(
     {
         "random": ".random <item1> <item2> ... <itemN>\
 \nUsage: Get a random item from the list of items."
     }
 )
-
 CMD_HELP.update(
     {
         "sleep": ".sleep <seconds>\
 \nUsage: Userbots get tired too. Let yours snooze for a few seconds."
     }
 )
-
 CMD_HELP.update(
     {
         "shutdown": ".shutdown\
@@ -151,35 +130,30 @@ CMD_HELP.update(
 hear Windows XP shutdown sound... but you don't."
     }
 )
-
 CMD_HELP.update(
     {
         "repo": ".repo\
 \nUsage: If you are curious what makes the userbot work, this is what you need."
     }
 )
-
 CMD_HELP.update(
     {
         "readme": ".readme\
 \nUsage: Provide links to setup the userbot and it's modules."
     }
 )
-
 CMD_HELP.update(
     {
         "repeat": ".repeat <no.> <text>\
 \nUsage: Repeats the text for a number of times. Don't confuse this with spam tho."
     }
 )
-
 CMD_HELP.update(
     {
         "restart": ".restart\
 \nUsage: Restarts the bot !!"
     }
 )
-
 CMD_HELP.update(
     {
         "raw": ".raw\
